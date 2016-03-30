3a1e42978bf13c50e3f16a5e8064d1cdbab7e269
dennis-hh <dennis.kopp84@googlemail.com>
2016-03-30 18:02:31 +0200

    switched to event-based system

--- README.md
+++ README.md
@@ -22,10 +22,10 @@ var manager = new RaumfeldManager();
 manager.discover();
 
 // fetch a device and do something with it. Method calls on a device return a promise object
-var device = manager.getDevice("Kitchen");
-
-device.getVolume().then(function(value) {
-    console.log("Device volume is " + value);
+manager.on("rendererFound", function(renderer) {
+    renderer.getVolume().then(function(value) {
+        console.log(renderer.name + " volume is " + value);
+    });
 });
 
 ```
--- example.js
+++ example.js
@@ -5,9 +5,9 @@ var manager = new RaumfeldManager();
 // starts discovering renderers asynchronously
 manager.discover();
 
-// fetch a renderer and do something with it. Method calls on a renderer return a promise object
-var renderer = manager.getRenderer("Kitchen");
-
-renderer.getVolume().then(function(value) {
-    console.log("Renderer volume is " + value);
+//listen for rendererFound event
+manager.on("rendererFound", function(renderer) {
+    renderer.getVolume().then(function(value) {
+        console.log(renderer.name + " volume is " + value);
+    });
 });
